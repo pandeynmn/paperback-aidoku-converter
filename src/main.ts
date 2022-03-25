8fa7c03c97f1197d9fa9c6899cdfd6a298f9311f
pandeynmn <namanpandey23@gmail.com>
2022-03-24 21:02:13 -0700

    Paperback to Aidoku Converter
    Paperback to Aidoku Converter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
+import * as fs from 'fs';
+import { convertPaperback } from './converter';
+
 if (require.main === module) {
     main().catch((err) => console.log(`Uncaught exception: \n\n${err}`));
 }
 
+// yarn build; yarn start;
 async function main() {
-    console.log('This is some cool async stuff');
+    console.log("Converting...");
+
+    // Importing backup
+    const importJson = fs.readFileSync('./input-output/paperback.json', 'utf8');
+
+    // Aidoku Backup JSON Object returned
+    const aidokuObject = convertPaperback(importJson);
+
+    // Write to file
+    const data = JSON.stringify(aidokuObject);
+    const date_str = new Date(Date.now()).toISOString().split('T')[0]
+    fs.writeFileSync(`./input-output/aidoku_${date_str}.json`, data)
+
+    console.log(`Done!\nCheck ./input-output/aidoku_${date_str}.json`);
 }
---
+++ src/types/paperback.ts
@@ -0,0 +1,111 @@
+// Generated using QuickType https://app.quicktype.io/
+
+export type PBBackup = {
+    library: Library[]
+    sourceMangas: SourceMangas[]
+    chapterMarkers: ChapterMarker[]
+    backupSchemaVersion: number
+    date: number
+    tabs: any[]
+    version: string
+    sourceRepositories: SourceRepository[]
+    activeSources: ActiveSource[]
+  }
+
+export interface ActiveSource {
+    author: string
+    desc: string
+    website: string
+    id: string
+    tags: ActiveSourceTag[]
+    repo: string
+    websiteBaseURL: string
+    version: string
+    icon: string
+    name: string
+}
+
+export interface ActiveSourceTag {
+    type: string
+    text: string
+}
+
+export interface Library {
+    lastRead: number
+    manga: Manga
+    lastUpdated: number
+    dateBookmarked: number
+    libraryTabs: any[]
+    updates: number
+}
+
+export interface Manga {
+    id:             string;
+    rating?:        number;
+    covers:         any[];
+    author:         string;
+    tags:           MangaTag[];
+    desc:           string;
+    titles:         string[];
+    image:          string;
+    additionalInfo: AdditionalInfo;
+    hentai:         boolean;
+    artist:         string;
+    status:         string;
+    banner?:        string;
+}
+
+export interface AdditionalInfo {
+    langFlag:  string;
+    users:     string;
+    langName:  string;
+    avgRating: string;
+    views:     string;
+    follows:   string;
+}
+
+export interface MangaTag {
+    id: string
+    label: string
+    tags: TagTag[]
+}
+
+export interface TagTag {
+    id: string
+    value: string
+}
+
+export interface SourceMangas {
+    mangaId: string
+    id: string
+    manga: Manga
+    originalInfo: Manga
+    sourceId: string
+}
+
+export interface SourceRepository {
+    name: string
+    url: string
+}
+
+export interface ChapterMarker {
+    totalPages: number;
+    lastPage:   number;
+    chapter:    Chapter;
+    completed:  boolean;
+    time:       number;
+    hidden:     boolean;
+}
+
+export interface Chapter {
+    chapNum:      number;
+    mangaId:      string;
+    volume:       number;
+    id:           string;
+    time:         number;
+    sortingIndex: number;
+    sourceId:     string;
+    group:        string;
+    langCode:     string;
+    name:         string;
+}
