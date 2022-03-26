45e60d2aac00773ae2e74294899a67999f16ac84
Groovin <admin@groovin.dev>
2022-03-25 22:00:32 -0400

    Medium sized updates
    - Take input/output from passed arguments instead of a folder
    - Updated functions to =>
    - Changed types to interfaces
    - Increased print width

---
+++ src/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { PBBackup, Library as PBLibray } from './paperback';
+export { AidokuBackup, Library as AidokuLibrary, Chapter as AidokuChapter, History as AidokuHistory, Manga as AidokuManga } from './aidoku';
--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export { PBBackup, Library as PBLibray } from "./paperback";
-export {
-    AidokuBackup,
-    Library as AidokuLibrary,
-    Chapter as AidokuChapter,
-    History as AidokuHistory,
-    Manga   as AidokuManga
-} from "./aidoku";
